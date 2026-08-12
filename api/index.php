3b150da3ad60ccf158fdb97ff40f8838df9ac6c8
Vercel Deployer <user@example.com>
2026-08-12 20:54:51 +0700

    fix: set storage path to /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,30 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0777, true);
+    mkdir($storagePath.'/app/public', 0777, true);
+    mkdir($storagePath.'/framework/cache/data', 0777, true);
+    mkdir($storagePath.'/framework/sessions', 0777, true);
+    mkdir($storagePath.'/framework/views', 0777, true);
+    mkdir($storagePath.'/logs', 0777, true);
+}
+
+putenv('LOG_CHANNEL=stderr');
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->useStoragePath($storagePath);
+
+$app->handleRequest(Request::capture());
